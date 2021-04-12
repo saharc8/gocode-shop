013cbc1b9eb37cf5ef1be4d98a065be592d9d008
saharc8 <saharc8@gmail.com>
2021-04-12 19:33:23 +0300

    initial commit

---
+++ src/MyButton.js
@@ -0,0 +1,5 @@
+const MyButton = () => {
+  return <button>click me</button>;
+};
+
+export default MyButton;
---
+++ src/Products.js
@@ -0,0 +1,20 @@
+import Product from "./Product";
+
+const Products = () => {
+  return (
+    <section className="products">
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+    </section>
+  );
+};
+
+export default Products;
