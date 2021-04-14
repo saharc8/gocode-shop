b176a798d3d2963ba88a185ec6a370dbf9ae1e45
saharc8 <saharc8@gmail.com>
2021-04-14 16:22:51 +0300

    targil-2

--- src/Product.js
+++ src/Product.js
@@ -1,15 +1,12 @@
-const Product = () => {
+const Product = ({ id, title, price, description, category, image }) => {
   return (
     <div className="product-card">
       <div className="product-image">
-        <img
-          src="https://cdn.shopify.com/s/files/1/0938/8938/products/10231100205_1_1315x1800_300_CMYK_1024x1024.jpeg?v=1445623369"
-          alt="pic"
-        />
+        <img src={image} alt="pic" />
       </div>
       <div className="product-info">
-        <h5>Winter Jacket</h5>
-        <h6>$99.99</h6>
+        <h5>{title}</h5>
+        <h6>{price}</h6>
       </div>
     </div>
   );
